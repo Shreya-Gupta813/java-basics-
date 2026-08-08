df58dc171afb307fbb8e88e166565dc8bc092b43
Shreya Gupta <shreya4it@gmail.com>
2026-08-08 16:57:16 +0530

    Simple Calculator

---
+++ simplecalculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class simplecalculator {
+    public static void main(String[] agrs) {
+        Scanner s = new Scanner(System.in);
+        double num1;
+        double num2;
+        String op;
+        double res = 0;
+        boolean validoperator = true;
+        System.out.print("Enter number 1: ");
+        num1 = s.nextDouble();
+        System.out.print("Enter the Operator(+,-,*,/,%,^): ");
+        op = s.next();
+        System.out.print("Enter number 2: ");
+        num2 = s.nextDouble();
+        switch (op) {
+            case "+" -> res = num1 + num2;
+            case "-" -> res = num1 - num2;
+            case "*" -> res = num1 * num2;
+            case "/" -> {
+                if (num2 == 0) {
+                    System.out.println("cannot divide by 0(zero)");
+                    validoperator = false;
+                } else {
+                    res = num1 / num2;
+                }
+            }
+            case "%" -> {
+                if (num2 == 0) {
+                    System.out.println("cannot divide by 0(zero)");
+                    validoperator = false;
+                } else {
+                    res = num1 % num2;
+                }
+            }
+            case "^" -> res = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator");
+                validoperator = false;
+            }
+        }
+        if (validoperator) {
+            System.out.println(res);
+        }
+    }
+}
