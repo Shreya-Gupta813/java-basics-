9eb2f841adb5b5e2d83668fb853575b685b37676
Shreya Gupta <shreya4it@gmail.com>
2026-08-08 11:49:53 +0530

    Add random number and find largest amoung them

---
+++ randomNumber.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class randomNumber {
+    public static void main(String args[]) {
+        Random rand = new Random();
+        int num1 = rand.nextInt(1, 101);
+        int num2 = rand.nextInt(1, 101);
+        int num3 = rand.nextInt(1, 101);
+        System.out.println("Random number: " + num1);
+        System.out.println("Random number: " + num2);
+        System.out.println("Random number: " + num3);
+        // if(num1>num2){
+        // if(num1>num3){
+        // System.out.println("Largest number: "+num1);
+        // }
+        // else{
+        // System.out.println("Largest number: "+num3);
+        // }
+        // }
+        // else{
+        // if(num2>num3){
+        // System.out.println("Largest number: "+num2);
+        // }
+        // else{
+        // System.out.println("Largest number: "+num3);
+        // }
+        // }
+        int largest = Math.max(num1, Math.max(num2, num3));
+        System.out.println("Largest number: " + largest);
+    }
+}
